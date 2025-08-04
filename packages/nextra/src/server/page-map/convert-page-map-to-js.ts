84c89c89c1bedc620717a2f7caa604688897ba6f
Paul <paul@theapplegates.com>
2025-08-04 16:53:53 -0400

    Restore convertPageMapToJs.ts

--- packages/nextra/src/server/page-map/convert-page-map-to-js.ts
+++ packages/nextra/src/server/page-map/convert-page-map-to-js.ts
@@ -1,11 +1,16 @@
+import { PageMapItem } from './types'
+
 export function convertPageMapToJs({
   pageMap,
   mdxPages,
   globalMetaPath
 }: {
-  pageMap: any;
-  mdxPages?: any;
-  globalMetaPath?: string;
-}): string {
-  return `export const pageMap = ${JSON.stringify(pageMap, null, 2)};`;
+  pageMap: PageMapItem[]
+  mdxPages: Record<string, any>
+  globalMetaPath: string
+}) {
+  const stringified = JSON.stringify(pageMap, null, 2)
+  return `export const pageMap = ${stringified}\n\n` +
+    `export const mdxPages = ${mdxPages ? JSON.stringify(mdxPages) : '{}'}\n\n` +
+    `export const globalMeta = ${globalMetaPath ? `'${globalMetaPath}'` : 'null'}\n`
 }
